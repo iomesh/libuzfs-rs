34cdd266ba0ff3ffd30903bdc22a320bc07bb633
sundengyu <dengyu.sun@iomesh.com>
2023-12-28 18:58:23 +0800

    feat(coroutine): implemented backtrace of all pending coroutines
    Signed-off-by: sundengyu <dengyu.sun@iomesh.com>

--- uzfs-sys/src/coroutine.rs
+++ uzfs-sys/src/coroutine.rs
@@ -1,4 +1,6 @@
 use crate::bindings::*;
+#[cfg(debug_assertions)]
+use backtrace::Symbol;
 use dashmap::DashMap;
 use futures::{Future, FutureExt};
 use once_cell::sync::OnceCell;
@@ -16,25 +18,137 @@ static uzfs_runtime: OnceCell<Runtime> = OnceCell::new();
 // start from 1 to pass null pointer check
 static current_task_id: AtomicU64 = AtomicU64::new(1);
 
+pub static add_backtrace: OnceCell<fn(u64, String)> = OnceCell::new();
+pub static remove_backtrace: OnceCell<fn(u64)> = OnceCell::new();
+pub static add_creation_pos: OnceCell<fn(u64, String)> = OnceCell::new();
+pub static remove_creation_pos: OnceCell<fn(u64)> = OnceCell::new();
+
+#[cfg(debug_assertions)]
+const MAX_BACKTRACE_DEPTH: u32 = 20;
+
 pub struct UzfsCoroutineFuture {
     uc: *mut uzfs_coroutine_t,
     pub task_id: u64,
+    #[cfg(debug_assertions)]
+    backtrace: bool,
 }
 
 type CoroutineFunc = unsafe extern "C" fn(arg: *mut c_void);
 
 impl UzfsCoroutineFuture {
     // FIXME(sundengyu): pin arg to the address of arg to prevent future swap
-    pub fn new(func: CoroutineFunc, arg: usize, foreground: bool) -> Self {
+    #[allow(unused_variables)]
+    pub fn new(func: CoroutineFunc, arg: usize, foreground: bool, backtrace: bool) -> Self {
         let task_id = current_task_id.fetch_add(1, Ordering::Relaxed);
-        let uc = unsafe {
-            libuzfs_new_coroutine(Some(func), arg as *mut c_void, task_id, foreground as u32)
+
+        #[cfg(not(debug_assertions))]
+        let record_backtrace = None;
+
+        #[cfg(debug_assertions)]
+        let record_backtrace = if backtrace {
+            if let Some(add_creation_pos_func) = add_creation_pos.get() {
+                add_creation_pos_func(task_id, UzfsCoroutineFuture::get_creation_pos(foreground));
+            }
+            Some(UzfsCoroutineFuture::record_backtrace as unsafe extern "C" fn(u64))
+        } else {
+            None
         };
-        UzfsCoroutineFuture { uc, task_id }
+
+        let uc = unsafe {
+            libuzfs_new_coroutine(
+                Some(func),
+                arg as *mut c_void,
+                task_id,
+                foreground as u32,
+                record_backtrace,
+            )
+        };
+        UzfsCoroutineFuture {
+            uc,
+            task_id,
+            #[cfg(debug_assertions)]
+            backtrace,
+        }
+    }
+
+    #[cfg(debug_assertions)]
+    fn parse_frame_symbol(symbol: &Symbol) -> (&str, u32) {
+        let filename = match symbol.filename() {
+            Some(path) => path.to_str().unwrap(),
+            None => "",
+        };
+
+        let lineno = symbol.lineno().unwrap_or(0);
+
+        (filename, lineno)
+    }
+
+    #[inline]
+    #[cfg(debug_assertions)]
+    fn get_creation_pos(foreground: bool) -> String {
+        let mut continue_trace = true;
+        let mut pos = String::new();
+        backtrace::trace(|frame| {
+            let mut ret = continue_trace;
+            backtrace::resolve_frame(frame, |symbol| {
+                let name = if let Some(name) = symbol.name() {
+                    name.as_str().unwrap()
+                } else {
+                    return;
+                };
+
+                if !ret {
+                    if !foreground && name.eq("taskq_create") {
+                        ret = true;
+                        return;
+                    }
+
+                    let (filename, lineno) = UzfsCoroutineFuture::parse_frame_symbol(symbol);
+                    pos = format!("{name} {filename}:{lineno}");
+                }
+
+                if (foreground && name.contains("UzfsCoroutineFuture::new"))
+                    || (!foreground && name.eq("zk_thread_create"))
+                {
+                    continue_trace = false;
+                }
+            });
+            ret
+        });
+        pos
     }
 
     unsafe extern "C" fn wake(arg: *mut c_void) {
         Box::from_raw(arg as *mut Waker).wake();
+    }
+
+    #[cfg(debug_assertions)]
+    unsafe extern "C" fn record_backtrace(task_id: u64) {
+        use std::fmt::Write;
+        if let Some(add_backtrace_func) = add_backtrace.get() {
+            let mut bt_string = String::from("");
+            let mut depth = 0;
+            let mut begin_record = false;
+            backtrace::trace(|frame| {
+                backtrace::resolve_frame(frame, |symbol| {
+                    let name = if let Some(name) = symbol.name() {
+                        name.as_str().unwrap()
+                    } else {
+                        return;
+                    };
+
+                    if begin_record {
+                        let (filename, lineno) = UzfsCoroutineFuture::parse_frame_symbol(symbol);
+                        writeln!(bt_string, "{depth} {name} {filename}:{lineno}").unwrap();
+                        depth += 1;
+                    } else if name.contains("record_backtrace") {
+                        begin_record = true;
+                    }
+                });
+                depth < MAX_BACKTRACE_DEPTH
+            });
+            add_backtrace_func(task_id, bt_string);
+        }
     }
 }
 
@@ -63,6 +177,16 @@ impl Future for UzfsCoroutineFuture {
 
 impl Drop for UzfsCoroutineFuture {
     fn drop(&mut self) {
+        #[cfg(debug_assertions)]
+        if self.backtrace {
+            if let Some(remove_backtrace_func) = remove_backtrace.get() {
+                remove_backtrace_func(self.task_id);
+            }
+            if let Some(remove_creation_pos_func) = remove_creation_pos.get() {
+                remove_creation_pos_func(self.task_id);
+            }
+        }
+
         unsafe { libuzfs_destroy_coroutine(self.uc) };
     }
 }
@@ -78,7 +202,7 @@ pub unsafe extern "C" fn thread_create(
     joinable: boolean_t,
     new_runtime: boolean_t,
 ) -> u64 {
-    let coroutine = UzfsCoroutineFuture::new(thread_func.unwrap(), arg as usize, false);
+    let coroutine = UzfsCoroutineFuture::new(thread_func.unwrap(), arg as usize, false, false);
     let task_id = coroutine.task_id;
 
     if new_runtime != 0 {
