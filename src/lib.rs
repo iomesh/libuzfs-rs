c8b63d6c312b99633b1772e52b8bc9477bbb41f0
sundengyu <dengyu.sun@iomesh.com>
2024-11-23 09:57:30 +0800

    feat(perf): implement lock contention perf tools
    Signed-off-by: sundengyu <dengyu.sun@iomesh.com>

--- src/lib.rs
+++ src/lib.rs
@@ -42,3 +42,53 @@ impl UzfsTestEnv {
         self.dev_file.as_file().set_len(new_size).unwrap();
     }
 }
+
+pub fn fetch_contention_graph(weight_threshold: u64) -> String {
+    use petgraph::dot::*;
+    let contentions = context::stack::fetch_lock_contentions();
+    let mut node_map = std::collections::HashMap::with_capacity(128);
+    let mut graph = petgraph::Graph::new();
+    graph.reserve_nodes(128);
+    graph.reserve_edges(1024);
+    for (bt, c) in contentions {
+        let nodes: Vec<_> = bt
+            .into_iter()
+            .map(|addr| {
+                if let Some(node) = node_map.get(&addr) {
+                    *node
+                } else {
+                    let mut symbol = String::new();
+                    unsafe {
+                        backtrace::resolve_unsynchronized(addr as *mut libc::c_void, |s| {
+                            if s.lineno().is_some() && s.name().is_some() {
+                                symbol = format!("{}:{}", s.name().unwrap(), s.lineno().unwrap());
+                            }
+                        })
+                    };
+                    let node = graph.add_node(symbol);
+                    node_map.insert(addr, node);
+                    node
+                }
+            })
+            .collect();
+
+        for i in 0..(nodes.len() - 1) {
+            if let Some(idx) = graph.find_edge(nodes[i], nodes[i + 1]) {
+                *graph.edge_weight_mut(idx).unwrap() += c
+            } else {
+                graph.add_edge(nodes[i], nodes[i + 1], c);
+            }
+        }
+    }
+
+    graph.retain_edges(|g, e| *g.edge_weight(e).unwrap() > weight_threshold);
+    graph.retain_nodes(|g, n| {
+        g.neighbors_directed(n, petgraph::Direction::Incoming)
+            .next()
+            .is_some()
+            || g.neighbors_directed(n, petgraph::Direction::Outgoing)
+                .next()
+                .is_some()
+    });
+    Dot::with_config(&graph, &[]).to_string()
+}
