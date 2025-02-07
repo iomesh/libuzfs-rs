cf5ea3ea31109c1397c2313c1a5aa311b75e0390
sundengyu <dengyu.sun@iomesh.com>
2025-02-07 15:34:18 +0800

    feat(aio): use lockless linked list to store aio tasks
    Signed-off-by: sundengyu <dengyu.sun@iomesh.com>

--- src/bin/bench/main.rs
+++ src/bin/bench/main.rs
@@ -60,7 +60,7 @@ async fn main() {
     uzfs_env_init().await;
     let dev_path = std::env::args().nth(1).unwrap();
     let sync: bool = std::env::args().nth(2).unwrap().parse().unwrap();
-    let concurrency = 64;
+    let concurrency = 48;
     let blksize = 1 << 20;
     let file_size = 1 << 30;
 
